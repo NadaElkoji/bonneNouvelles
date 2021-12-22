3547226ee081dfb3b14d39b497f256a4821e6e0e
Nada Elkoji <nada.elkoji@student.vinci.be>
2021-12-22 19:33:32 +0100

    Refactor PositivityController :: Error countPositivity

--- PositivityControllerTest.php
+++ PositivityControllerTest.php
@@ -17,13 +17,6 @@ class PositivityControllerTest extends TestCase{
         $this->assertEquals("test", "test");
     }
 
-    public function test_countPositivity_givenBon_shouldReturnCorrect(){
-
-        $actual = $this->positivityController->countPositivity("bon");
-
-        $this->assertEquals("correct", $actual);
-    }
-
     public function test_countPositivity_givenMatin_shouldReturnNeutre(){
 
         $actual = $this->positivityController->countPositivity("matin");
