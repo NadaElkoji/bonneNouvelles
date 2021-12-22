5489b6ff3801a8835655efee688eec5a94307e85
Nada Elkoji <nada.elkoji@student.vinci.be>
2021-12-22 09:37:21 +0100

    Add files

---
+++ views/home.php
@@ -0,0 +1,5 @@
+<div id="contenu">
+	<h2>Accueil Good news </h2>
+	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
+	<p>Jean-Luc Collinet.</p>
+</div><!-- #contenu -->
