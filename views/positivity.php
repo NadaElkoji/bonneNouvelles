d497b3a03e50d3aefca52d4768e7daec6f112413
Nada Elkoji <nada.elkoji@student.vinci.be>
2021-12-22 16:31:45 +0100

    Create positivity view

--- views/positivity.php
+++ views/positivity.php
@@ -1,1 +1,7 @@
-<?php
+<div id="contenu">
+    <form action="index.php?action=contact" method="post">
+
+        <p><textarea rows="3" name="message">Entrez vos mots</textarea></p>
+        <p><input type="submit" name="form_contact" value="Envoyer"></p>
+    </form>
+</div>
